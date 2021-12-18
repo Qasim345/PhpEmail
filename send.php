32260d1a7fd958a12586d22f6e7dd2d84ec34683
Qasim <69038179+Qasim345@users.noreply.github.com>
2021-12-18 13:40:19 +0430

    update send.php

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ $subject = "Mail from users";
 $txt = "Name = ".$name."\r\n Email = ".$email."\r\n Message = ".$message;
 $headers = "From: qasim@qasim-af.herokuapp.com \r\n";
 mail($to, $subject, $txt, $headers);
-/*if ($su) {
+if ($su) {
   header("location: index.php");
-}*/
+}
 ?>
