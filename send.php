f88b380cc88906ed34e61d7b439a57a3073e7953
Qasim <69038179+Qasim345@users.noreply.github.com>
2021-12-18 11:54:35 +0430

    update send.php

--- send.php
+++ send.php
@@ -6,5 +6,8 @@ $to = "qasimsarwari2020@gmail.com";
 $subject = "Mail from users";
 $txt = "Name = ".$name."\r\n Email = ".$email."\r\n Message = ".$message;
 $headers = "From: qasimexam@example.com \r\n";
-mail($to, $subject, $txt, $headers);
+$su = mail($to, $subject, $txt, $headers);
+if ($su) {
+  header("location: index.php");
+}
 ?>
